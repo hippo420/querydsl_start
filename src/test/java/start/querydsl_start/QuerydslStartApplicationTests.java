c9294c34d6cfc0f3ab3d4533f8cdb98b650057cf
hippo420 <hippo420@naver.com>
2025-07-16 20:49:44 +0900

    Initial commit

---
+++ src/test/java/start/querydsl_start/QuerydslStartApplicationTests.java
@@ -0,0 +1,13 @@
+package start.querydsl_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
