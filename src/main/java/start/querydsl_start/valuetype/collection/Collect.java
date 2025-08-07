7bb60572edc7d577d0258a932fab374281e02071
hippo420 <hippo420@naver.com>
2025-08-07 21:33:51 +0900

    식별관계 - embed

--- src/main/java/start/querydsl_start/valuetype/collection/Collect.java
+++ src/main/java/start/querydsl_start/valuetype/collection/Collect.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import start.querydsl_start.valuetype.embeded.Period;
 
 import java.util.ArrayList;
 import java.util.List;
